78daeb88b34098f7d34507771ec7b73d03cbe34f
Klaudia <kosaa96@gmail.com>
2023-01-09 22:16:01 +0100

    create gql mutation for register

--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -1,13 +1,27 @@
 import { Button } from "../components/Button";
 import { MainTemplate } from "../templates/MainTemplate";
+import { useFormik } from "formik";
+import { useState } from "react";
 
 const Register = () => {
+  const [registerError, setRegisterError] = useState();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <MainTemplate>
       <>
         <div className="container-fluid pt-3">
           <h2 className="text-center pb-2">Sign up</h2>
-          <form className="mx-auto w-25">
+          <form className="mx-auto w-25" onSubmit={formik.handleSubmit}>
             <div className="mb-2">
               <label
                 htmlFor="email"
@@ -20,14 +34,22 @@ const Register = () => {
                 type="email"
                 className="form-control"
                 id="email"
-                aria-describedby="emailHelp"
+                aria-describedby="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
               />
             </div>
             <div className="mb-3">
               <label htmlFor="password" className="form-label">
                 Password
               </label>
-              <input type="password" className="form-control" id="password" />
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+              />
             </div>
             <Button
               text="Sign up"
